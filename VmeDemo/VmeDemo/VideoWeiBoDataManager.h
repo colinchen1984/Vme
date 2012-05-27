c5d20e08b137b1fdb3dd3390dd7004cad205e03b
colin <beyond0924@gmail.com>
2012-05-27 23:16:03 +0800

    add video's weibo data manager

---
+++ VmeDemo/VmeDemo/VideoWeiBoDataManager.h
@@ -0,0 +1,13 @@
+//
+//  VideoWeiBoDataManager.h
+//  VmeDemo
+//
+//  Created by user on 12-5-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoWeiBoDataManager : NSObject
+
+@end
---
+++ VmeDemo/VmeDemo/VideoWeiBoDataManager.m
@@ -0,0 +1,13 @@
+//
+//  VideoWeiBoDataManager.m
+//  VmeDemo
+//
+//  Created by user on 12-5-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "VideoWeiBoDataManager.h"
+
+@implementation VideoWeiBoDataManager
+
+@end
