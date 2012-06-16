024ef73ec73376f0b86909266589462e7f1bd0f0
colin <beyond0924@gmail.com>
2012-06-16 14:16:56 +0800

    完成基础功能,实现微博的界面

---
+++ VmeDemo/VmeDemo/SendWeiBoView.h
@@ -0,0 +1,13 @@
+//
+//  SendWeiBoView.h
+//  VmeDemo
+//
+//  Created by user on 12-6-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendWeiBoView : UIView
+
+@end
---
+++ VmeDemo/VmeDemo/SendWeiBoView.m
@@ -0,0 +1,31 @@
+//
+//  SendWeiBoView.m
+//  VmeDemo
+//
+//  Created by user on 12-6-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SendWeiBoView.h"
+
+@implementation SendWeiBoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
