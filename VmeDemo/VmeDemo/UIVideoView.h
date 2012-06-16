d1e9d964e316149c1ff9718ae35894e8247ca0cd
colin <beyond0924@gmail.com>
2012-06-16 19:29:50 +0800

    切换版本

---
+++ VmeDemo/VmeDemo/UIVideoView.h
@@ -0,0 +1,13 @@
+//
+//  UIVideoView.h
+//  VmeDemo
+//
+//  Created by user on 12-6-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIVideoView : UIView
+
+@end
---
+++ VmeDemo/VmeDemo/UIVideoView.m
@@ -0,0 +1,31 @@
+//
+//  UIVideoView.m
+//  VmeDemo
+//
+//  Created by user on 12-6-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIVideoView.h"
+
+@implementation UIVideoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
